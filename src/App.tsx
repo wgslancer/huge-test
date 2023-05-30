4a271a9225e5eb0c64dc3cd85da84f2c992ddfa6
wgslancer <71307509+wgslancer@users.noreply.github.com>
2023-05-31 02:12:42 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Homepage from 'src/pages/Homepage/components/HomepageRoute';
 function App() {
   return (
     <div className="App min-h-screen w-full">
+      <div>Hello</div>
       <Homepage />
     </div>
   );
